9142e0b8b743ae7d193ced69deb51cfdba554532
Srini Koganti <skoganti@gmail.com>
2012-09-08 18:13:56 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trnjob::Application.config.secret_token = '13d6e558aa2d20b0418fbe7339157c5036fb11a5136a7d93c01c464e36d59962156213f83bdb37f91292fa295dab947aaafdcad211bbb46cb9d9471a07a3c006'
